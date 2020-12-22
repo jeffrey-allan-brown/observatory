02ce72cfb1ba9e2336deddd6c37b8eb7ca955f12
hieronymous-bean <jeffrey@jeffreyallanbrown.com>
2020-12-21 21:52:02 -0500

    Dashboard and routing config

--- src/components/RequestCard.vue
+++ src/components/RequestCard.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
   <b-card
-    img-alt="Image"
+    :img-alt="name"
+    :img-src="thumbnail"
     img-top
     tag="request"
     :title="name"
     :subTitle="subtitle"
     style=""
-    class="shadow-sm col px-1"
+    class="shadow-sm border col py-3"
   >
-    <b-button href="#" variant="dark" size="lg" class="mt-3">Use this API</b-button>
+    <b-button href="#" variant="dark" size="lg" class="mt-3 stretched-link">Use this API</b-button>
   </b-card>
 </div>
 </template>
@@ -17,6 +18,6 @@
 <script>
 export default {
   name: 'RequestCard',
-  props: [ 'name', 'subtitle' ]
+  props: [ 'name', 'subtitle', 'thumbnail' ]
 }
 </script>
